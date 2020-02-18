6b6dcb8398ef6844f3711e2936ce424557ce385e
biancardona <biancardona.rivera@gmail.com>
2020-02-18 11:37:47 -0600

    extname file

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
+const path = require('path');
+//let mdLinks = {};
 
+var ext = path.extname('index.js');
+console.log(ext);
 
-fs.readFile('README.md', 'utf8',(err, data) => {
-  if (err)
-  return console.log(err);
-     console.log(data) ;
-});
-console.log('the file is read');
+if (ext === '.md') {
+  fs.readFile('README.md', 'utf8',(err, data) => {
+    if (err) { throw err; }
+    console.log('data: ', data);
+  });
+  console.log('the file is read');
+}
+
--- mdlinks.js
+++ mdlinks.js
@@ -1,12 +1,15 @@
-const fs = require('fs')
+// const fs = require('fs');
+
 
 module.exports = function mdLinks(filePath, options) {
     console.log(filePath);
+
+
     //Leer archivos
     // Leer si es .md
     //Validar si el archivo es md
     //Parsear archivo y encontrar un link
-    //Checar si validate == true entonces 
+    //Checar si validate == true entonces
         //Validar link
     //Checar si stats == true entonces
         //Agregar el resultado a los stats
