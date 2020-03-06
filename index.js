f2389c757c46fb68695563327d3d00aa92567d0d
biancardona <biancardona.rivera@gmail.com>
2020-03-06 11:44:21 -0600

    updating function readFile with util.promisify for promises

--- index.js
+++ index.js
@@ -2,79 +2,63 @@ const fs = require('fs');
 const path = require('path');
 const mdLinks = process.argv[2];
 const fetch = require('node-fetch');
+const util = require("util");
 
-let validLinks = []
-let invalidLinks = []
-
-//Regex global para leer los links
+//*** Regex global para leer los links ***
 const regexMdLinks = /\[([^\[]+)\](\(.*\))/gm
 
-//Para verificar la extension del archivo
+//*** Para verificar la extension del archivo ***
 const ext = path.extname(mdLinks);
-//console.log(ext);
-
 if (ext === '.md') {
-  fs.readFile(mdLinks, 'utf8', (err, data) => {
-    const matches = data.match(regexMdLinks)
-
-    //Regex para obtener los links con el texto
-    const singleMatch = /\[([^\[]+)\]\((.*)\)/
-    for (var i = 0; i < matches.length; i++) {
-      var text = singleMatch.exec(matches[i])
-
-      // Visualizacion de los links mas el texto con el retorno de la REGEX
-      regexResult = (`#${i} File:  ${mdLinks} Text: ${text[1]} Link: ${text[2]}`);
-      console.log(regexResult)
-      //console.log(result);
-
-      // //Usando fetch para hacer peticion de extraer datos (link)
-      fetch(text[2])
-        .then(checkStatus)
-        .then(function (data) {
-          console.log('Request succeeded with JSON response', data);
-        }).catch(function (error) {
-          console.log('Request failed', error);
-        });
-    }
-    if (err) {
-      throw err;
+  const readFile = util.promisify(fs.readFile);
+  readFile(mdLinks, 'utf8')
+    .then((text) => {
+      console.log(text);
+  const matches = text.match(regexMdLinks)
+  //*** Regex para obtener los links con el texto ***
+  const singleMatch = /\[([^\[]+)\]\((.*)\)/
+  for (var i = 0; i < matches.length; i++) {
+    let text = singleMatch.exec(matches[i])
+    // *** Visualizacion de los links mas el texto con el retorno de la REGEX ***
+    linkResult = (text[2])
+    regexResult = (`#${i} File:  ${mdLinks} Text: ${text[1]} Link: ${text[2]}`);
+    console.log(linkResult);
     }
   });
 } else {
   console.log('Is not a .MD file');
 }
-// Funcion para checar el status del link
-function checkStatus(response) {
-  if (response.checkStatus >= 200 && response.checkStatus < 300) {
-    validLinks.push(response.url)
-    return Promise.resolve(response)
-  } else {
-    invalidLinks.push(response.url)
-    return Promise.reject(new Error(response.statusText))
+
+/*let validLinks = []
+let invalidLinks = []
+/*const checkStatus = async (text[2]) => {
+  try {
+    const response = await fetch(text[2])
+    if (response.status < 400) {
+      validLinks.push(response.url)
+      return Promise.resolve(response)
+    } else {
+      invalidLinks.push(response.url)
+      return await Promise.reject(new Error(response.statusText))
+    }
+  } catch (error) {
+    console.log('Request failed', error);
   }
-}
+};
+checkStatus()
+  .catch(function (error) {
+    console.log('Request failed', error);
+  });
+//Usando fetch para hacer peticion de extraer datos (link)
+// Funcion para checar el status del link
+//console.log(validLinks)
+//console.log(invalidLinks)
+console.log('Valid Links: ', validLinks)
+console.log('Invalid Links: ', invalidLinks)
 
-// function checkStatus(res) {
-//   //  console.log(res)
-//   if (res.status < 400) {
-//     //console.log(res.status)
-//     validLinks.push(res.url)
-//   } else {
-//     invalidLinks.push(res.url)
-//     //let err = new Error(res.statusText)
-//     //err.response = res
-//     //throw err
-//   }
+/*Promise.all(checkStatus).then(() => {
 
-//   console.log('Valid Links: ', validLinks)
-//   console.log('Invalid Links: ', invalidLinks)
-
-// }
-
-
-//Promise.all(checkStatus).then(() => {
-
-/* if (process.argv[3] === '--validate') {
+ if (process.argv[3] === '--validate') {
     console.log(validLinks);
   } else if (process.argv[3] === '--stats') {
 
